86843db20697807f783d3f299b112a1483e87482
Manuel Haugg <manuhak8s>
2023-03-20 19:23:57 +0100

    file handling changes

--- flask/helper.py
+++ flask/helper.py
@@ -8,5 +8,5 @@ fshare_path = '../fshare/result.yaml'
 # dummy data for testing visualization on streamlit
 # gets written to the fileshare and read from the frontend
 dummy_result = {
-    'result': ['bank a', 'bank b', 'bank c']
+    'result': ['bank a', 'bank b', 'bank d']
 }
